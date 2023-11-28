58d7ef706a3b1d3ac57eb9913bf072ef3767fb8a
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-11-28 10:45:21 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["dog", "cat", "cop", "jogger", "driver", "taxi driver"];
+  let action = [
+    "ate my",
+    "took my",
+    "broke my",
+    "stole my",
+    "bit my",
+    "spit on my"
+  ];
+  let item = ["homework", "car", "shoe", "leg"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let itemIndex = Math.floor(Math.random() * item.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    item[itemIndex]
+  );
 };
